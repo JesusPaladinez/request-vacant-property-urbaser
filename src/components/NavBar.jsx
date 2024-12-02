1d4793dd76513f10f9b2033e198fd64e677a6423
Jesús Paladinez <jesuspaladinez18@gmail.com>
2024-12-02 17:29:25 -0500

    Online support icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+
 export default function NavBar() {
   return (
-    <div className='bg-blue-600 flex justify-between px-5 py-4 text-xl'>
+    <div className='bg-blue-600 flex justify-start px-5 py-4 text-xl gap-4'>
+      <i class="fa-solid fa-headset text-white"></i>
       <p className='text-white'>Atención en Línea</p>
     </div>
   )
