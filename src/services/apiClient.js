a56fafb2fdc0c637a727f31d3ea1e5838ab6839b
Jesús Paladinez <jesuspaladinez18@gmail.com>
2025-01-15 14:49:05 -0500

    Deployed API route

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://backendrequestvacantproperty.onrender.com/api",
 });
 
 export default apiClient;
