dc529960d72911dd771631f61aa0878d44fceef1
Jesús Paladinez <jesuspaladinez18@gmail.com>
2025-01-31 16:33:03 -0500

    WhatsApp from the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
       <ul className='flex flex-col gap-4 text-white p-4'>
         <li><a href="https://urbaser.co/popayan" target='_blank' rel='noopener noreferrer' className='hover:text-urbaser-2 flex items-center gap-x-2'><LuLink2 className='text-urbaser-2 hover:text-white text-xl'/>https://urbaser.co/popayan</a></li>
         <li><a href="mailto:notificacionesoficiales.popayan@urbaser.co" className='hover:text-urbaser-2 flex items-center gap-x-2'><HiOutlineMail className='text-urbaser-2 hover:text-white text-xl' />notificacionesoficiales.popayan@urbaser.co</a></li>
-        <li><a href="tel:3112403041" className='hover:text-urbaser-2 flex items-center gap-x-2'><ImPhone className='text-urbaser-2 hover:text-white text-base' />Llamadas y WhatsApp: <span className='font-medium'>311 240 3041</span></a></li>
+        <li><a href="https://wa.me/573112403041" target='_blank' rel='noopener noreferrer' className='hover:text-urbaser-2 flex items-center gap-x-2'><ImPhone className='text-urbaser-2 hover:text-white text-base' />Llamadas y WhatsApp: <span className='font-medium'>311 240 3041</span></a></li>
       </ul>
     </footer>
   )
